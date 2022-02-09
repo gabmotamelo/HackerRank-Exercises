221f007d5abdd74595cd7662fb1614a663949fda
Gabriel Mota <gabrielmota6@hotmail.com>
2022-02-09 12:00:35 -0300

    min-max-sum

---
+++ Min-Max-Sum/minMaxSum.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function miniMaxSum(arr) {
+    let sum = arr.reduce((a,b) => {
+        return a + b;
+    });
+    const min = sum - Math.max(...arr);
+    const max = sum - Math.min(...arr);
+    console.log(min + ' ' + max);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
